f93aa756c6c63d6b4e13264de5f526094f1495f1
MariySemenenko <mashyni92@gmail.com>
2023-10-12 18:43:29 +0300

    creatr code for loader

--- src/redux/catalog/catalogSlice.js
+++ src/redux/catalog/catalogSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
-import api from "redux/operations/Api/api";
+import { createSlice } from '@reduxjs/toolkit';
+import api from 'redux/operations/Api/api';
 
-const handlePending = (state) => {
+const handlePending = state => {
   state.isLoading = true;
 };
 const handleRejected = (state, action) => {
@@ -10,73 +10,168 @@ const handleRejected = (state, action) => {
 };
 
 const catalogSlice = createSlice({
-  name: "catalog",
+  name: 'catalog',
   initialState: {
     adverts: [],
     page: 1,
     isLoading: false,
     error: null,
     filters: {
-      selectedMake: "",
-      selectedPrice: "",
-      minMileage: "",
-      maxMileage: "",
+      selectedMake: '',
+      selectedPrice: '',
+      minMileage: '',
+      maxMileage: '',
     },
     filtersFavorite: {
-      selectedMake: "",
-      selectedPrice: "",
-      minMileage: "",
-      maxMileage: "",
+      selectedMake: '',
+      selectedPrice: '',
+      minMileage: '',
+      maxMileage: '',
     },
-   
   },
   reducers: {
     firstAdverts: (state, action) => {
-      
+      state.isLoading = true;
+
       state.adverts = [...state.adverts, ...action.payload];
       state.page = state.page + 1;
     },
     setAdverts: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
       state.adverts = [...state.adverts, ...action.payload];
     },
-    onNextPage: (state) => {
-     
+    onNextPage: state => {
+      state.isLoading = false;
+      state.error = null;
       state.page = state.page + 1;
     },
     setFilters: (state, action) => {
+      state.isLoading = false;
       state.filters = {
         ...state.filters,
         ...action.payload,
       };
     },
     setFiltersFavorite: (state, action) => {
+      state.isLoading = false;
       state.filtersFavorite = {
         ...state.filtersFavorite,
         ...action.payload,
       };
     },
-    setIsLoading: (state, action) => {
-      state.isLoading = action.payload; 
+    // Виконається в момент старту HTTP-запиту
+    fetchingInProgress(state) {
+      state.isLoading = true;
     },
-
-    [api.pending]: handlePending,
-
-    [api.fulfilled](state, action) {
+    // Виконається якщо HTTP-запит завершився успішно
+    fetchingSuccess(state) {
       state.isLoading = false;
       state.error = null;
-      state.items = action.payload;
     },
-    [api.rejected]: handleRejected,
-
+    // Виконається якщо HTTP-запит завершився з помилкою
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
 export const {
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
   setAdverts,
   onNextPage,
   firstAdverts,
   setFilters,
   setFiltersFavorite,
-  setIsLoading,
+  isLoading,
+  error,
 } = catalogSlice.actions;
 export default catalogSlice.reducer;
+
+//
+
+// import { createSlice } from "@reduxjs/toolkit";
+// import api from "redux/operations/Api/api";
+
+// const handlePending = (state) => {
+//   state.isLoading = true;
+// };
+// const handleRejected = (state, action) => {
+//   state.isLoading = false;
+//   state.error = action.payload;
+// };
+
+// const catalogSlice = createSlice({
+//   name: "catalog",
+//   initialState: {
+//     adverts: [],
+//     page: 1,
+//     isLoading: false,
+//     error: null,
+//     filters: {
+//       selectedMake: "",
+//       selectedPrice: "",
+//       minMileage: "",
+//       maxMileage: "",
+//     },
+//     filtersFavorite: {
+//       selectedMake: "",
+//       selectedPrice: "",
+//       minMileage: "",
+//       maxMileage: "",
+//     },
+
+//   },
+//   reducers: {
+//     firstAdverts: (state, action) => {
+
+//       state.adverts = [...state.adverts, ...action.payload];
+//       state.page = state.page + 1;
+//     },
+//     setAdverts: (state, action) => {
+//       state.adverts = [...state.adverts, ...action.payload];
+//     },
+//     onNextPage: (state) => {
+
+//       state.page = state.page + 1;
+//     },
+//     setFilters: (state, action) => {
+//       state.filters = {
+//         ...state.filters,
+//         ...action.payload,
+//       };
+//     },
+//     setFiltersFavorite: (state, action) => {
+//       state.filtersFavorite = {
+//         ...state.filtersFavorite,
+//         ...action.payload,
+//       };
+//     },
+//     setIsLoading: (state, action) => {
+//       state.isLoading = action.payload;
+//     },
+
+//     [api.pending]: handlePending,
+
+//     [api.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items = action.payload;
+//     },
+//     [api.rejected]: handleRejected,
+
+//   },
+// });
+
+// export const {
+//   setAdverts,
+//   onNextPage,
+//   firstAdverts,
+//   setFilters,
+//   setFiltersFavorite,
+//   setIsLoading,
+// } = catalogSlice.actions;
+// export default catalogSlice.reducer;
--- src/redux/operations/Api/api.jsx
+++ src/redux/operations/Api/api.jsx
@@ -1,4 +1,11 @@
 import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
+} from "../../catalog/catalogSlice";
+
 
 axios.defaults.baseURL = 'https://65217a54a4199548356d42c6.mockapi.io';
 const limit = 8;
@@ -11,5 +18,21 @@ async function api(page) {
     console.error(error);
   }
 }
+
+//
+// export const api = () => async dispatch => {
+//   try {
+//     // Індикатор завантаження
+//     dispatch(fetchingInProgress());
+//     // HTTP-запит
+//     const response = await axios.get("/tasks");
+//     // Обробка даних
+//     dispatch(fetchingSuccess(response.data));
+//   } catch (e) {
+//     // Обробка помилки
+//     dispatch(fetchingError(e.message));
+//   }
+// };
+//
 
 export default api;
