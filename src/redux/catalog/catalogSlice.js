d523c096321b563b1b561ae4b2b6a46f90a4519e
MariySemenenko <mashyni92@gmail.com>
2023-10-13 19:56:26 +0300

    delete comits

--- src/redux/catalog/catalogSlice.js
+++ src/redux/catalog/catalogSlice.js
@@ -1,89 +1,4 @@
 
-// import { createSlice } from '@reduxjs/toolkit';
-// import api from 'redux/operations/Api/api';
-
-// const handlePending = (state) => {
-//   state.isLoading = true;
-// };
-// const handleRejected = (state, action) => {
-//   state.isLoading = false;
-//   state.error = action.payload;
-// };
-
-// const catalogSlice = createSlice({
-//   name: 'catalog',
-//   initialState: {
-//     adverts: [],
-//     page: 1,
-//     isLoading: false,
-//     error: null,
-//     filters: {
-//       selectedMake: '',
-//       selectedPrice: '',
-//       minMileage: '',
-//       maxMileage: '',
-//     },
-//     filtersFavorite: {
-//       selectedMake: '',
-//       selectedPrice: '',
-//       minMileage: '',
-//       maxMileage: '',
-//     },
-//   },
-//   reducers: {
-//     firstAdverts: (state, action) => {
-//       state.adverts = [...state.adverts, ...action.payload];
-//       state.page = state.page + 1;
-//     },
-//     setAdverts: (state, action) => {
-//       state.error = null;
-//       state.adverts = [...state.adverts, ...action.payload];
-//     },
-//     onNextPage: state => {
-//       state.error = null;
-//       state.page = state.page + 1;
-//     },
-//     setFilters: (state, action) => {
-//       state.filters = {
-//         ...state.filters,
-//         ...action.payload,
-//       };
-//     },
-//     setFiltersFavorite: (state, action) => {
-//       state.filtersFavorite = {
-//         ...state.filtersFavorite,
-//         ...action.payload,
-//       };
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//     .addCase(api.pending, handlePending)
-//       .addCase(api.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.error = null;
-//         state.adverts = [...state.adverts, ...action.payload];
-//       })
-//       .addCase(api.rejected, handleRejected)
-//   },
-// });
-
-// export const {
- 
-//   setAdverts,
-//   onNextPage,
-//   firstAdverts,
-//   setFilters,
-//   setFiltersFavorite,
-//   isLoading,
-//   error,
-// } = catalogSlice.actions;
-// export default catalogSlice.reducer;
-
-
-
-
-
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from 'redux/operations/Api/api';
 
@@ -193,17 +108,84 @@ export default catalogSlice.reducer;
 
 //
 
-// import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit';
+
+// import { createSlice } from '@reduxjs/toolkit';
 // import api from 'redux/operations/Api/api';
 
-// export const fetchAdverts = createAsyncThunk('catalog/fetchAdverts', async (_, { rejectWithValue }) => {
-//   try {
-//     const response = await api.fetchAdverts(); 
-//     return response.data;
-//   } catch (error) {
-//     return rejectWithValue(error.message);
-//   }
+// const handlePending = (state) => {
+//   state.isLoading = true;
+// };
+// const handleRejected = (state, action) => {
+//   state.isLoading = false;
+//   state.error = action.payload;
+// };
+
+// const catalogSlice = createSlice({
+//   name: 'catalog',
+//   initialState: {
+//     adverts: [],
+//     page: 1,
+//     isLoading: false,
+//     error: null,
+//     filters: {
+//       selectedMake: '',
+//       selectedPrice: '',
+//       minMileage: '',
+//       maxMileage: '',
+//     },
+//     filtersFavorite: {
+//       selectedMake: '',
+//       selectedPrice: '',
+//       minMileage: '',
+//       maxMileage: '',
+//     },
+//   },
+//   reducers: {
+//     firstAdverts: (state, action) => {
+//       state.adverts = [...state.adverts, ...action.payload];
+//       state.page = state.page + 1;
+//     },
+//     setAdverts: (state, action) => {
+//       state.error = null;
+//       state.adverts = [...state.adverts, ...action.payload];
+//     },
+//     onNextPage: state => {
+//       state.error = null;
+//       state.page = state.page + 1;
+//     },
+//     setFilters: (state, action) => {
+//       state.filters = {
+//         ...state.filters,
+//         ...action.payload,
+//       };
+//     },
+//     setFiltersFavorite: (state, action) => {
+//       state.filtersFavorite = {
+//         ...state.filtersFavorite,
+//         ...action.payload,
+//       };
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//     .addCase(api.pending, handlePending)
+//       .addCase(api.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.error = null;
+//         state.adverts = [...state.adverts, ...action.payload];
+//       })
+//       .addCase(api.rejected, handleRejected)
+//   },
 // });
 
-
-//
+// export const {
+ 
+//   setAdverts,
+//   onNextPage,
+//   firstAdverts,
+//   setFilters,
+//   setFiltersFavorite,
+//   isLoading,
+//   error,
+// } = catalogSlice.actions;
+// export default catalogSlice.reducer;
